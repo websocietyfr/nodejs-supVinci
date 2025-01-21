a5cadb0705d034d4e0f61dfccd64ec83a8124365
Soufian AIT TIRITE <contact@soufian-a.net>
2025-01-21 11:16:40 +0100

    add cors support

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const initRoutes = require('./routes')
 // const initModels = require('./models')
 require('dotenv').config()
 global.UPLOAD_DIR = __dirname + '/assets/uploads/';
+const cors = require('cors');
 
+const corsOptions = {
+    credentials: true,
+    origin: ['*'] // Whitelist the domains you want to allow
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 
 initRoutes(app)
